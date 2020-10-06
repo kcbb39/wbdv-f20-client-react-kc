81388f07dc387db1860694cfdeadc53f5cf7dedf
Kayla Caputo <caputo.k@husky.neu.edu>
2020-10-06 19:05:10 -0400

    Hello World Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div classname='container-fluid'>
+      <h1>Hello World!</h1>
+      <h3>How are you?</h3>
+   </div>
   );
 }
 
