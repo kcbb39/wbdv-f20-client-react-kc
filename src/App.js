f7f1949fc3a24c6cfe56f3490ab160ef2213db48
Kayla Caputo <caputo.k@husky.neu.edu>
2020-10-06 22:21:05 -0400

    Lecture sept 29

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css'
-import HelloWorld from './components/HelloWorld'
+import CourseCard from './components/CourseCard'
+import ModuleList from './components/ModuleList'
+
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-   <div classname='container-fluid'>
-      <h1>Hello World!</h1>
-      <h3>How are you?</h3>
+   <div className="container">
+      <h1>Whiteboard</h1>
+      <ModuleList/>
+       <div className="card-deck">
+         <CourseCard/>
+         <CourseCard/>
+       </div>
    </div>
   );
 }
